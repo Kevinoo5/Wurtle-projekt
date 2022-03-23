89b86b07acc2de6f07463e7040d73f2a808e2f86
Kevinoo5 <87882945+Kevinoo5@users.noreply.github.com>
2022-03-23 15:39:24 +0200

    Add files via upload

--- poggers.py
+++ poggers.py
@@ -1,7 +1,8 @@
 import random
 sonad = []
 count = 0
-blanks = "-----"
+blanks = ["-","-","-","-","-"]
+letters = []
 with open("stufff.txt") as f:
 
     for line in f.readlines():
@@ -9,16 +10,21 @@ with open("stufff.txt") as f:
         sonad.append(line)
 
 suvaline = random.choice(sonad).lower()
-print(suvaline)
 true = False
 while count < 5:
-    if true != True:
-        proov = input("Sisestage sõna: ")
-    for i in range(5):
-        if proov == suvaline and true != True:
-            print("U are so smarterst")
-            true = True
-            break
-        elif proov[i] == suvaline[i] and true == False:
-            blanks[i].replace(proov[i])
-    count += 1
+    proov = input("Sisestage sõna: ")
+    if len(proov) == 5:
+        for i in range(5):
+            if proov == suvaline:
+                print("U are so smarterst")
+                true = True
+                exit()
+            elif proov[i] == suvaline[i] and true == False:
+                blanks[i] = proov[i]
+                count += 1
+        for letter in proov:
+            letters.append(letter)
+        print(letters)
+        print("Correct letters: " + letters[i])
+        print("".join(blanks))
+print(suvaline)
